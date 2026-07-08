bc28c4656d02a4917847fd207bdd9e5febfab3b5
marinagosson <marinagosson@snowmanlabs.com>
2026-07-08 19:54:07 -0300

    chore: add dist

---
+++ dist/config/database/database.js
@@ -0,0 +1,17 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+function required(name) {
+    const value = process.env[name];
+    if (!value) {
+        throw new Error(`Environment variable ${name} is not defined.`);
+    }
+    return value;
+}
+export const database = new Pool({
+    host: process.env.PGHOST ?? required("DB_HOST"),
+    port: Number(process.env.PGPORT ?? required("DB_PORT")),
+    database: process.env.PGDATABASE ?? required("DB_NAME"),
+    user: process.env.PGUSER ?? required("DB_USER"),
+    password: process.env.PGPASSWORD ?? required("DB_PASSWORD"),
+});
--- dist/repositories/movieVectorRepository.js
+++ dist/repositories/movieVectorRepository.js
@@ -1,4 +1,4 @@
-import { database } from "../config/database.js";
+import { database } from "../config/database/database.js";
 class MovieVectorRepository {
     async search(searchVector) {
         const client = await database.connect();
